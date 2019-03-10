38276dcbc668e862b558ec5d5e259aa9df41059b
Vedran Linić <toolrijeka@gmail.com>
2019-03-10 18:09:00 +0100

    added search functionality

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
+
 //Define variables
 
-let input;
+const form =    `<form>
+                    <input type="text" name="search" id="search" placeholder="Search">
+                </form>`;
+const $links;                
 
+//append input form to header
 
-input = $(":input").val();
-input.toLowerCase();
+$(".header").html(form);
+
+//Search function
+
+$links = $("a"); 
+
+$("input").on("keyup", event => {
+  let val = event.target.value;
+  for (let i = 0; i < $links.length; i += 1) {
+    var $submition = $links[i].getAttribute("data-title");
+    if ($submition.toLowerCase().indexOf(val.toLowerCase()) != -1) {
+      $links[i].style.display = "block";
+    } else {
+      $links[i].style.display = "none";
+    }
+  }
+});
